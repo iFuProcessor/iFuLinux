8346b5835bada012af2e08787fd375dc31591720
liusiyuan2022 <louis.ryant2022@gmail.com>
2024-08-10 13:54:26 +0000

    feat: img show "yuanshen"

--- initrd_pck32/iFu/img_show.sh
+++ initrd_pck32/iFu/img_show.sh
@@ -1,9 +1,13 @@
-# fudan.bin citrus.bin mumu.bin
+# fudan.bin citrus.bin mumu.bin white.bin yuanshen.bin
 while true; do
     cat ./imgs/fudan.bin > /dev/nt35510
-    sleep 4
+    sleep 6
     cat ./imgs/citrus.bin > /dev/nt35510
-    sleep 4
+    sleep 6
     cat ./imgs/mumu.bin > /dev/nt35510
-    sleep 4
+    sleep 6
+    cat ./imgs/white.bin > /dev/nt35510
+    sleep 6
+    cat ./imgs/yuanshen.bin > /dev/nt35510
+    sleep 6
 done
